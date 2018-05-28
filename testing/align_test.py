4990e5cdc968d5978baab7d48370aff06174d0f9
gabefoley <gabriel.foley@uqconnect.edu.au>
2018-05-28 20:52:03 +1000

    Removed testing folder

--- testing/align_test.py
+++
@@ -1,7 +0,0 @@
-
-import utilities
-import alignment
-
-filepath = "/Users/gabefoley/Dropbox/PhD/Projects/2U1/2U1_2018/Excluding plants fungi nematodes insects and bacteria/180312_fifty_percent_identity/Further_exclusions/April_2018/CYP2U1_CYP2R1/CYP2U1_CYP2R1.fasta"
-
-alignment.align_with_mafft(filepath, "./")
--- testing/write_csv_test.py
+++
@@ -1,8 +0,0 @@
-import csv
-names= {"dog" : "harold", "cat" : "charlie"}
-
-with open("./test_csv.csv", "w+") as file:
-    for animal, name in names.items():
-        file.write(name + ",")
-        file.write("is a " + ",")
-        file.write(animal)
